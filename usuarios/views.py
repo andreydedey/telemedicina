9b488c1a1c1f70f6726d29e75928aa92cdbb8a2a
Andrey Oliveira <dedey.cardoso@gmail.com>
2024-04-16 19:42:03 -0300

    Static files and template files

--- usuarios/views.py
+++ usuarios/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 def cadastro(request):
     print(request.META)
-    return HttpResponse('Ola Mundo!')
+    return render(request, 'cadastro.html')
